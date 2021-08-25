df12a27dfdf2e86b089f9696eca18747cde249f6
alefred <alfredo.arias@outlook.com>
2021-08-24 22:43:48 -0500

    Updating with env variable and validation rules

--- arm/iac-orange.ps1
+++
@@ -1,8 +0,0 @@
-# iniciar el arm template con: arm!
-$rg = 'orange_rg'
-
-New-AzResourceGroupDeployment `
-    -name 'IaC-Orange' `
-    -TemplateFile 'iac-orange.json' `
-    -webappName 'orangeApp00' `
-    -ResourceGroupName $rg 
