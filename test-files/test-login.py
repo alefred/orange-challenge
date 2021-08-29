e3a11a27a37699d3dc2f9200889f15c2848ae5d5
alefred <alfredo.arias@outlook.com>
2021-08-28 22:19:33 -0500

    works good

--- test-files/test-login.py
+++ test-files/test-login.py
@@ -7,8 +7,8 @@ chrome_opt = webdriver.ChromeOptions()
 chrome_opt.add_argument('--headless')
 chrome_opt.add_argument('--no-sandbox')
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=chrome_opt)
+
 print "Driver Made"
-#driver.get(url)
 driver.get("http://" + str(sys.argv[1]))
 print "URL got"
 driver.implicitly_wait(5)
@@ -18,15 +18,13 @@ username_textbox.send_keys("greg")
 
 password_textbox = driver.find_element_by_id("password")
 password_textbox.send_keys("turnquist")
-#login_button = driver.find_element_by_css_selector(".btn-block").click()
 login_button = driver.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-block']")
 login_button.click()
 print("Login successful: " + driver.current_url)
 print("Page source" + driver.page_source)
 driver.close()
 
-#driver.get("http://oczoom.azurewebsites.net")  / orangeappfree01.azurewebsites.net
-#//*[text()='Sign in']
-#//*[text()='Sign in'][1]
+#python ./test-files/test-login.py orangeappfree01.azurewebsites.net 
 
+//div[@role="alert"]
 
