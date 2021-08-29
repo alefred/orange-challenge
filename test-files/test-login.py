b201c39a175ea0534714c7c823ea0bacd541723e
alefred <alfredo.arias@outlook.com>
2021-08-29 16:34:05 -0500

    Updating the documentation

--- test-files/test-login.py
+++ test-files/test-login.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 import sys 
 
